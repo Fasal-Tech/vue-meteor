38c8dee0a88f812843cc211499fe36d2bc3ee143
MrSpark2591 <mr.spark2591@gmail.com>
2024-11-06 16:00:30 +0530

    added old vue package in repo

---
+++ packages/vue/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+name: 'mrspark:vue',
+version: '1.0.0',
+summary: 'Integrate Vue with Meteor',
+git: 'https://github.com/Akryum/meteor-vue-component',
+documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+api.versionsFrom('3.0.4');
+api.use('mrspark:npm-check@1.0.0');
+api.use('ecmascript');
+api.mainModule('index.js', 'client');
+api.export('Vue', 'client');
+});
+
+Npm.depends({
+'lodash.omit': '4.5.0',
+});
